d35cdae6b4856b98c1b392022536b4dd1cfe73a1
DeM0NChiCk <toropov.116gh@gmail.com>
2026-02-15 13:36:57 +0300

    object + enum

---
+++ ObjectFirstLesson/src/module/Product.java
@@ -0,0 +1,40 @@
+package module;
+
+public class Product {
+    public static int counterProducts = 0;
+    private String name;
+    private int position;
+    private int count;
+
+    {
+        counterProducts++;
+    }
+
+    public Product(String name, int position, int count) {
+        this.name = name;
+        this.position = position;
+        this.count = count;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPosition(){
+        return position;
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void addOne(){
+        this.count++;
+    }
+
+    public void removeOne(){
+        if (this.count > 0)
+            this.count--;
+    }
+
+}
