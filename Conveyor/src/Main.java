22c1dfe76e90d7fc09cb86a7bb50d7936c38eab0
DeM0NChiCk <toropov.116gh@gmail.com>
2026-02-28 15:52:41 +0300

    Generics

---
+++ Conveyor/src/Main.java
@@ -0,0 +1,9 @@
+import enums.FragilityLevel;
+import model.FragileBox;
+
+public class Main {
+    public static void main(String[] args) {
+        FragileBox fragileBox = new FragileBox(100, 100, 100, FragilityLevel.MAX);
+
+    }
+}
